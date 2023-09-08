710298adfda58bb99803a87e140b2df49d44454d
Lukas Linde <linde.lukas.99@gmail.com>
2023-09-08 19:22:28 +0200

    corrected

--- Statistik/Kundenstatistik.sql
+++ Statistik/Kundenstatistik.sql
@@ -10,7 +10,7 @@ AND (b.Rechnungsbetrag > 0 OR b.Rechnungsdatum > CURRENT_DATE)
 GROUP BY k.Mailadresse, b.Buchungsnummer;
 
 CREATE VIEW Kundenstatistik AS
-SELECT k.Mailadresse, k.Kundenname, COUNT(b.Buchungsnummer) AS AnzahlBuchungen, COUNT(s.Stornierungsnummer) AS AnzahlStornierungen, SUM(z.Zahlung) AS Gesamtzahlung
+SELECT k.Mailadresse, k.Kundenname, COUNT(b.Buchungsnummer) AS AnzahlBuchungen, COUNT(s.Stornierungsnummer) AS AnzahlStornierungen, ABS(SUM(z.Zahlung)) AS Gesamtzahlung
 FROM Kunde k left outer join Buchung b on k.Mailadresse = b.Mailadresse
 left outer join StornierteBuchung s on k.Mailadresse = s.Mailadresse
 left outer join KundeZahlung z on k.Mailadresse = z.Mailadresse
