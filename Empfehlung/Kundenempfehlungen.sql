bc5e0b09e2993980f317149b7bc933bb55132fca
lulinde <linde.lukas.99@gmail.com>
2024-05-21 03:10:39 +0200

    Only one view for Task 6

--- Empfehlung/Kundenempfehlungen.sql
+++ Empfehlung/Kundenempfehlungen.sql
@@ -1,14 +1,6 @@
 -- Task 6 c)
 
-CREATE VIEW KundenempfehlungenOne AS
-SELECT k.Mailadresse, f.Wohnungsnummer, b.Rating
-FROM Kunde k
-INNER JOIN Buchung b ON k.Mailadresse = b.Mailadresse
-INNER JOIN Ferienwohnung f ON b.Wohnungsnummer = f.Wohnungsnummer
-WHERE b.Rating >= 4
-GROUP BY k.Mailadresse, f.Wohnungsnummer, b.Rating;
-
-CREATE VIEW KundenempfehlungenTwo AS
+CREATE VIEW Kundenempfehlungen AS
 SELECT k.Mailadresse, f.Wohnungsnummer, b.Rating
 FROM Kunde k
 INNER JOIN Buchung b ON k.Mailadresse = b.Mailadresse
@@ -17,6 +9,6 @@ WHERE b.Rating >= 4
 GROUP BY k.Mailadresse, f.Wohnungsnummer, b.Rating;
 
 SELECT K1.Mailadresse AS Kunde1, K2.Mailadresse AS Kunde2, K2.Wohnungsnummer AS EmpfohleneWohnung
-FROM KundenempfehlungenOne K1
-INNER JOIN KundenempfehlungenTwo K2 ON K1.Wohnungsnummer = K2.Wohnungsnummer AND K1.Rating = K2.Rating
+FROM Kundenempfehlungen K1
+INNER JOIN Kundenempfehlungen K2 ON K1.Wohnungsnummer = K2.Wohnungsnummer AND K1.Rating = K2.Rating
 WHERE K1.Rating = 5 AND K1.Mailadresse != K2.Mailadresse
